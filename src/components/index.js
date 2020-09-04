5767e21bed5668d573ea1ad609c948c1a1a50efb
Diarrah <diarrahcasiano@gmail.com>
2020-09-03 22:33:40 -0700

    Grouping for easy import

---
+++ src/components/index.js
@@ -0,0 +1,25 @@
+export { default as Navbar } from './Navbar';
+export { default as PageLinks } from './PageLinks';
+
+// Home
+export { default as HeroHomepage } from './HeroHomepage';
+export { default as Welcome } from './Welcome';
+export { default as HomepageCTA } from './HomepageCTA';
+export { default as Featured } from './Featured';
+export { default as FeaturedPortfolioLinks } from './FeaturedPortfolioLinks';
+
+// Portfolio
+export { default as PortfolioLink } from './PortfolioLink';
+
+// About
+export { default as AboutHero } from './AboutHero';
+export { default as OurHeritage } from './OurHeritage';
+export { default as Leaders } from './Leaders';
+export { default as LeaderCard } from './LeaderCard';
+
+// Contact
+export { default as ContactHero } from './ContactHero';
+export { default as ContactDetails } from './ContactDetails';
+export { default as ContactForm } from './ContactForm';
+
+export { default as Footer } from './Footer';
