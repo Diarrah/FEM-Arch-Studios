22bf8edaaecb84a29b060f535bf6f9308f4e794a
Diarrah <diarrahcasiano@gmail.com>
2020-09-13 16:34:16 -0700

    Setting classNames for mobile nav dynamically with useState

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,69 +1,56 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { PageLinks } from '../components';
 import logo from '../images/archLogo.svg';
 
 const Navbar = () => {
-    let mobileMenu = useRef(),
-        overlay = useRef(),
-        { body } = document;
+    const [menuOpen, setMenuOpen] = useState(false),
+          mobileNavBtn = useRef(),
+          { body } = document;
 
     useEffect(() => {
         hideMobileNav()
     }, [])
 
-    const hideMobileNav = () => {
-        let links = [...mobileMenu.current.children[0].children],
-            symbol = document.querySelector('.navbar--mobile');
-
-        links.forEach(link => link.addEventListener('click', () => {
-            overlay.current.classList.remove('active')
-            mobileMenu.current.classList.remove('deployed')
-            symbol.classList.remove('change')
-            body.style.overflowY = 'visible'; 
-            body.parentElement.style.overflowY = 'visible';          
-        }))
-    }
-
-    const mobileNavOpen = e => {
-        e.target.parentElement.classList.toggle('change');
-        e.target.nextSibling.classList.remove('spin');
-        mobileMenu.current.classList.toggle('deployed');
-        overlay.current.classList.add('active');
+    const mobileNavOpen = () => {
+        mobileNavBtn.current.classList.add('change');
         body.style.overflowY = 'hidden';
         body.parentElement.style.overflowY = 'hidden';
     }
 
-    const mobileNavClosed = e => {
+    const mobileNavClosed = () => {
         setTimeout(function() {
-            e.target.parentElement.classList.toggle('change')
+            mobileNavBtn.current.classList.remove('change')
         }, 850);
-        e.target.classList.add('spin');
-        mobileMenu.current.classList.toggle('deployed');
-        mobileMenu.current.style.transitionDelay = '450ms';
-        overlay.current.classList.remove('active');
-        overlay.current.style.transitionDelay = '450ms';
         body.style.overflowY = 'visible';
         body.parentElement.style.overflowY = 'visible';
-        e.persist();
+    }
+
+    const hideMobileNav = () => {
+        let links = [...document.querySelector('.navbar__links--mobile').children[0].children];
+
+        links.forEach(link => link.addEventListener('click', () => {
+            setMenuOpen(false)
+            mobileNavClosed()         
+        }));
     }
 
     return (
         <div className="navbar">
-            <Link to="/">
+            <Link to="/" onClick={() => setMenuOpen(false)}>
                 <img className="navbar__logo" src={logo} alt="Arch company logo" />
             </Link>
             <div className="navbar__links">
                 <PageLinks />
             </div>
-            <a href="#" className="navbar--mobile" aria-label="Button for mobile menu deployment">
+            <a href="#" className="navbar--mobile" ref={mobileNavBtn} aria-label="Button for mobile menu deployment" onClick={() => setMenuOpen(!menuOpen)}>
                 <i className="fa fa-bars" aria-hidden="true" onClick={mobileNavOpen} />
-                <i className="fa fa-times" aria-hidden="true" onClick={mobileNavClosed}  />
+                <i className={`fa fa-times ${!menuOpen ? 'spin' : ''}`} aria-hidden="true" onClick={mobileNavClosed}  />
             </a>
-            <div className="navbar__links--mobile" ref={mobileMenu} onClick={hideMobileNav}>
+            <div className={`navbar__links--mobile ${menuOpen ? 'deployed' : ''}`} onClick={hideMobileNav}>
                 <PageLinks />
             </div>
-            <div className="overlay" ref={overlay} />
+            <div className={`overlay ${menuOpen ? 'active' : ''}`} />
         </div>
     )
 }
